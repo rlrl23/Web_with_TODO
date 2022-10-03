c915e809042e78c462f46c68b89a0d0cb0b3d9ad
Luibov <rlrl23@mail.ru>
2022-10-03 10:00:41 +0500

    Route, Link

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_WHITELIST = [
+    "http://192.168.3.3:3000",
+    "http://192.168.3.3",
     "http://127.0.0.1",
     "http://127.0.0.1:3000",
     "http://localhost",
--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -1,14 +1,14 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer, ModelSerializer
 from mainapp.models import Project, Todo
 
 
-class ProjectModelSerializer(HyperlinkedModelSerializer):
+class ProjectModelSerializer(ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
 
 
-class TodoModelSerializer(HyperlinkedModelSerializer):
+class TodoModelSerializer(ModelSerializer):
     class Meta:
         model = Todo
         fields = '__all__'
